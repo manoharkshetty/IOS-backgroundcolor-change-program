602a754056b0d7f40e0f22a2e4205eb3c664b098
Manohar <manohar@vetris-macbook.home>
2014-06-15 19:54:11 +0530

    initial commit

---
+++ ColorChanger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorChanger
+//
+//  Created by Manohar on 09/06/14.
+//  Copyright (c) 2014 Manohar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
